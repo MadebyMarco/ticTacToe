48c9507c713424124571ff277e36cae823471d54
MadebyMarco <marco3xmedina@gmail.com>
2023-04-01 09:47:08 -0700

    create Board pieces index

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
-    let positions = [];
+    let piecesOnBoard = [];
+    let piecesOnBoardIndexes = [];
     
     const create = () => {
         const body = document.querySelector("body");
@@ -10,6 +11,7 @@ const gameBoard = (() => {
             square.classList.add("square");
             square.dataset.index = `${i}`;
             square.addEventListener("click", (e) => {
+                addSquareIndexToBoardIndex(e)
                 addSquareIndexToPlayer(e)
                 markSquare(e);
             })
@@ -21,13 +23,17 @@ const gameBoard = (() => {
     const clear = () => {
         const squares = document.querySelectorAll(".square");
         squares.forEach(square => square.textContent = "");
-        gameBoard.positions = ["X","O","X","O","X","O","X","O","X"];
+        gameBoard.piecesOnBoard = ["X","O","X","O","X","O","X","O","X"];
     }
 
     const isPlayer1Turn = () => {
-        if(positions[0] == undefined || positions[positions.length - 1] == "O") {
+        if(piecesOnBoard[0] == undefined || piecesOnBoard[piecesOnBoard.length - 1] == "O") {
             return true
         } else return false;
+    }
+
+    const addSquareIndexToBoardIndex = (e) => {
+        piecesOnBoardIndexes.push(e.target.dataset.index);
     }
 
     const addSquareIndexToPlayer = (e) => {
@@ -46,25 +52,25 @@ const gameBoard = (() => {
             if(isPlayer1Turn()) {
                 const piece = player1.pieces.pop()
                 e.target.textContent = piece ;
-                positions.push(piece);
+                piecesOnBoard.push(piece);
             } else {
                 const piece = player2.pieces.pop()
                 e.target.textContent = piece ;
-                positions.push(piece);
+                piecesOnBoard.push(piece);
             }
         } 
     }
 
     const remove = () => {
         document.querySelector(".board").remove();
-        gameBoard.positions = ["X","O","X","O","X","O","X","O","X"];
+        gameBoard.piecesOnBoard = ["X","O","X","O","X","O","X","O","X"];
     }
 
     const render = () => {
-        for(let i = 0; i < positions.length; i++) {
-            const positionSquare = document.querySelector(`div[data-index="${i + 1}"]`);
-            const shape = positions[i];
-            positionSquare.textContent = `${shape}`;
+        for(let i = 0; i < piecesOnBoard.length; i++) {
+            const piecesOnBoard = document.querySelector(`div[data-index="${i + 1}"]`);
+            const shape = piecesOnBoard[i];
+            piecesOnBoard.textContent = `${shape}`;
         }
     }
 
@@ -73,14 +79,13 @@ const gameBoard = (() => {
         clear,
         render,
         remove,
-        isPlayer1Turn,
-        positions
+        piecesOnBoard
     } 
 })();
 
 
-//for players we can have shape(X or O) and we can also have its positions on the board. 
-//It can have 9 possible positions, 1-9
+//for players we can have shape(X or O) and we can also have its piecesOnBoard on the board. 
+//It can have 9 possible piecesOnBoard, 1-9
 const player = (playerName, shape) => {
     let pieces = [];
     if(shape == "X") {
@@ -96,11 +101,11 @@ const player1 = player("player1", "X");
 const player2 = player("player2", "O");
 
 
-//Once a player reaches a certain pattern of positions, such as 1,2,3 = the top row, then the winnder will be announced;
+//Once a player reaches a certain pattern of piecesOnBoard, such as 1,2,3 = the top row, then the winnder will be announced;
 
 
 const gameFlow = (() => {
-    //Base turns off of positions 
+    //Base turns off of piecesOnBoard 
     const turn = player1
 })();
 
